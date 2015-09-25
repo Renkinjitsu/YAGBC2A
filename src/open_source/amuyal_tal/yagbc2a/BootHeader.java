71975c0c112026b12522f806f4c2527ca00cec85
Tal Amuyal <renkinjitsu@gmail.com>
2015-09-26 01:38:15 +0300

    Add a license notice at the beginning of each `.java` file.

--- src/open_source/amuyal_tal/yagbc2a/BootHeader.java
+++ src/open_source/amuyal_tal/yagbc2a/BootHeader.java
@@ -1,3 +1,23 @@
+/*
+    Yet Another Gameboy Classic/Color Assembler (YAGBC2A) can compile Gameboy-compatible images.
+    Copyright (C) 2015  Tal Amuyal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Contact information (email): TalAmuyal@gmail.com
+*/
+
 package open_source.amuyal_tal.yagbc2a;
 
 import java.util.ArrayList;
@@ -5,9 +25,9 @@ import java.util.List;
 
 import open_source.amuyal_tal.yagbc2a.cartridege.InterruptHandler;
 import open_source.amuyal_tal.yagbc2a.cartridege.Linker;
+import open_source.amuyal_tal.yagbc2a.cartridege.Linker.KnownSymbols;
 import open_source.amuyal_tal.yagbc2a.cartridege.MemorySection;
 import open_source.amuyal_tal.yagbc2a.cartridege.ResetHandler;
-import open_source.amuyal_tal.yagbc2a.cartridege.Linker.KnownSymbols;
 import open_source.amuyal_tal.yagbc2a.instruction.InstructionTemplate;
 import open_source.amuyal_tal.yagbc2a.utils.Utils;
 import open_source.amuyal_tal.yagbc2a.utils.adt.BytesArray;
